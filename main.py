6efab4ea228a01cbb896b741b2f97c306168bfa1
Bimal <trisky650@gmail.com>
2023-10-16 09:50:37 +0530

    Added time function to check elapsed time to copy clipboard

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import time
+from typing import Union
+
 import pyperclip
-from typing import  Union
+
 
 def copy_to_clipboard(user_input: Union[str, int]) -> None:
     pyperclip.copy(str(user_input))
-    print("Copied to clipboard")
 
+
+def waiting_time(user_input: Union[str, int]) -> None:
+    init = time.time()
+    copy_to_clipboard(user_input)
+    elapsed_time = time.time() - init
+    print("Copied to clipboard")
+    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+    if time.time() - init > 5:
+        print("Time is up")
+    else:
+        pass
 
 if __name__ == "__main__":
     user_input = input("Enter your text or number: ")
     copy_to_clipboard(user_input)
-
-
+    waiting_time(user_input)
     
