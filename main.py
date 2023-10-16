8fa8259530e64e3930c8cc6f8a5638c9622ee335
Bimal <trisky650@gmail.com>
2023-10-16 11:31:47 +0530

    Added dataclasses to add functionality

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-import time
-from typing import Union
-
 import pyperclip
+from typing import Union
+import time
+from dataclasses import dataclass
 
 
-def copy_to_clipboard(user_input: Union[str, int]) -> None:
-    pyperclip.copy(str(user_input))
+@dataclass
+class Clipboard:
+    text: str
+    num: int
+    user_input: Union[str, int, bytes]
+    time: float = 0.0
 
 
-def waiting_time(user_input: Union[str, int]) -> None:
-    init = time.time()
-    copy_to_clipboard(user_input)
-    elapsed_time = time.time() - init
-    print("Copied to clipboard")
-    print(f"Elapsed time: {elapsed_time:.2f} seconds")
+    def copy_to_clipboard(self, user_input: Union[str, int, bytes]) -> None:
+        pyperclip.copy(str(user_input))
 
-    if time.time() - init > 5:
-        print("Time is up")
-    else:
-        pass
+
+    def waiting_time(self, user_input: Union[str, int]) -> None:
+        init = time.time()
+        self.copy_to_clipboard(user_input)
+        elapsed_time = time.time() - init
+        print("Copied to clipboard")
+        print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+        if time.time() - init > 5:
+            print("Time is up")
+        else:
+            pass
 
 if __name__ == "__main__":
     user_input = input("Enter your text or number: ")
-    copy_to_clipboard(user_input)
-    waiting_time(user_input)
+    clipboard = Clipboard(text="example", num=1, user_input=user_input)
+    clipboard.waiting_time(user_input)
     
