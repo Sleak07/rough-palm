3c55031dd103795a9b64b0eb9ed7564b6a955fa1
Bimal <trisky650@gmail.com>
2024-01-27 23:57:52 +0530

    image crate addition

--- scarpy/src/main.rs
+++ scarpy/src/main.rs
@@ -1,3 +1,19 @@
+// file opener for image files
+
+use std::path::Path;
+use anyhow::Result;
+use image::io::Reader;
+
+fn get_image_dimensions(file_path: &str) -> Result<(u32, u32)> {
+    let path = Path::new(file_path);
+    let reader = Reader::open(path)?;
+    let dimensions = reader.into_dimensions()?;
+    Ok(dimensions)
+}    
 fn main (){
-    println!("Hello rust");
+    match get_image_dimensions("data/sample.jpg") {
+        Ok((width, height)) => println!("dimensions: {} x {}", width, height),
+        Err(e) => println!("error: {}", e),
+    }
+    
 }
