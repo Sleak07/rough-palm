c5b764e4b696083d8ef768252682d756be7d151b
Bimal <trisky650@gmail.com>
2024-01-27 18:02:16 +0530

    Removed scraping stuff

--- scarpy/src/main.rs
+++ scarpy/src/main.rs
@@ -1,24 +1,3 @@
 fn main (){
-
-    let response = reqwest :: blocking:: get("https://demo.opencart.com");// getting 
-
-    let data = response.unwrap().text().unwrap();
-
-
-    println!("{data}")
-    
-
-    // using structs to store data
-
-        struct DemoProduct {
-        image: Option<String>,
-        url: Option<String>,
-        title: Option<String>,
-        description: Option<String>,
-        new: Option<String>,
-        tax: Option<String>,
-    }
-    let mut demo_products: Vec<DemoProduct> = Vec::new();
-    
-    
+    println!("Hello rust");
 }
