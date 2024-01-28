4fa5c8cf890cb2bc44bb88093de4f9c2e576263b
Bimal <trisky650@gmail.com>
2024-01-28 11:51:00 +0530

    Returned the color type

--- scarpy/src/main.rs
+++ scarpy/src/main.rs
@@ -2,7 +2,14 @@
 use image ::GenericImageView;
 
 fn main(){
+    // opening the image file
     let img = image::open("galaxy.png").unwrap();
 
+    // getting the image file dimensions
     println!("dimensions {:?}", img.dimensions());
+
+    // getting the image color
+    println!("{:?}",img.color());
+
+    
 }
