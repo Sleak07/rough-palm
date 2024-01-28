80a620397627759c4b61c7a5c1c76001c7653c86
Bimal <trisky650@gmail.com>
2024-01-28 11:38:31 +0530

    added one image file and got its dimensions

--- scarpy/src/main.rs
+++ scarpy/src/main.rs
@@ -1,19 +1,8 @@
 // file opener for image files
+use image ::GenericImageView;
 
-use std::path::Path;
-use anyhow::Result;
-use image::io::Reader;
+fn main(){
+    let img = image::open("galaxy.png").unwrap();
 
-fn get_image_dimensions(file_path: &str) -> Result<(u32, u32)> {
-    let path = Path::new(file_path);
-    let reader = Reader::open(path)?;
-    let dimensions = reader.into_dimensions()?;
-    Ok(dimensions)
-}    
-fn main (){
-    match get_image_dimensions("data/sample.jpg") {
-        Ok((width, height)) => println!("dimensions: {} x {}", width, height),
-        Err(e) => println!("error: {}", e),
-    }
-    
+    println!("dimensions {:?}", img.dimensions());
 }
